c094a57faa0281dc8ff00cd76bafb0bd3bf2daff
ArianMirza <ArianAghamirza@gmail.com>
2025-04-06 02:08:24 +0200

    Added moving background gif and changed window sizes in regards to the  sidebar, also aligning the contact form center on pc windows and phone  view.

--- About.js
+++ About.js
@@ -1,48 +1,21 @@
- // Ensure the script runs after the DOM is fully loaded
- window.onload = function() {
-  const data = {
-    
-      labels: [
-          'Javascript',
-          'HTML',
-          'CSS',
-          'Designing',
-          'Coding',
-          'Cycling',
-          'Running'
+const data = {
+    labels: ['JS', 'CSS', 'HTMl'],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
       ],
-      datasets: [{
-          data: [65, 59, 90, 81, 56, 55, 40],
-          fill: true,
-          backgroundColor: 'rgb(255, 255, 255)',
-          borderColor: 'rgb(255, 255, 255)',
-          pointBackgroundColor: 'rgb(255, 255, 255)',
-          pointBorderColor: 'rgb(255, 255, 255)',
-          pointHoverBackgroundColor: 'rgb(255, 255, 255)',
-          pointHoverBorderColor: 'rgb(255, 255, 255)'
-      }, {
-    
-      }]
+      hoverOffset: 4
+    }]
   };
 
   const config = {
-    
-      type: 'radar',
-      data: data,
-      options: {
-          elements: {
-            
-              line: {
-                
-                  borderWidth: 3
-              }
-          }
-      }
+    type: 'doughnut',
+    data: data,
   };
 
-  // Get the canvas element
   const ctx = document.getElementById('myChart').getContext('2d');
-
-  // Create the chart
   new Chart(ctx, config);
-};
